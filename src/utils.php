f4d9d1c562d8c7f50220572de13c8e46161a5478
Spollo <damivise@gmail.com>
2018-03-30 23:09:24 +0200

    Fixed version of materialize

--- src/utils.php
+++ src/utils.php
@@ -25,9 +25,12 @@ function loginMessage($msg){
 function addMaterialize(){
   echo '<!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+	<link rel="stylesheet prefetch" href="shttps://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"></script>
 
     <!-- MaterializeIcons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">';
@@ -84,7 +87,7 @@ function addFooter(){
 ';
 }
 
-function addSidenav($pagetitle){
+function addSidenav(){
   echo '<nav>
 		
 		  <ul id="slide-out" class="side-nav">
