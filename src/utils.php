0c1fe1e466acaa4555566c4b2adab80b0e41793f
Spollo <damivise@gmail.com>
2018-03-30 23:00:20 +0200

    Added function to write HTML code of the side nav

--- src/utils.php
+++ src/utils.php
@@ -84,4 +84,16 @@ function addFooter(){
 ';
 }
 
+function addSidenav($pagetitle){
+  echo '<nav>
+		
+		  <ul id="slide-out" class="side-nav">
+			<li><a href="#!">First Sidebar Link</a></li>
+			<li><a href="#!">Second Sidebar Link</a></li>
+		  </ul>
+		  <a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="mdi-navigation-menu"></i></a>
+		</nav>
+		<script  src="scripts/sidenav.js"></script>';
+}
+
 ?>
