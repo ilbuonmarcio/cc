ebc25f1e61f86c8489b29ce432fc0370c82b8631
marcioz98 <marciozgaming@gmail.com>
2018-03-31 18:14:32 +0200

    Added feature block's placeholders

--- src/index.php
+++ src/index.php
@@ -20,5 +20,34 @@
 
     <h3 class="center-align">Benvenuto, <?php echo $_SESSION["username"]; ?>!</h3>
 
+    <div class="container">
+      <div class="row">
+        <div class="col s12 m6 offset-m3 center-align feature-block">
+          <i class="large material-icons">person_add</i>
+          <h5>Aggiungi Nuovo Account</h5>
+        </div>
+        <div class="col s12 m6 offset-m3 center-align feature-block">
+          <i class="large material-icons">cloud_upload</i>
+          <h5>Carica CSV Alunni</h5>
+        </div>
+        <div class="col s12 m6 offset-m3 center-align feature-block">
+          <i class="large material-icons">settings</i>
+          <h5>Configura Parametri CC</h5>
+        </div>
+        <div class="col s12 m6 offset-m3 center-align feature-block">
+          <i class="large material-icons">control_point</i>
+          <h5>Genera CC</h5>
+        </div>
+        <div class="col s12 m6 offset-m3 center-align feature-block">
+          <i class="large material-icons">visibility</i>
+          <h5>Visualizza CC</h5>
+        </div>
+        <div class="col s12 m6 offset-m3 center-align feature-block">
+          <i class="large material-icons">file_download</i>
+          <h5>Scarica CC</h5>
+        </div>
+      </div>
+    </div>
+
   </body>
 </html>
