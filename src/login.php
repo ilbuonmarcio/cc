65389c33566fd4e5154379fd5a78a0663f421648
marcioz98 <marciozgaming@gmail.com>
2018-04-04 16:37:33 +0200

    Removed unusual '

--- src/login.php
+++ src/login.php
@@ -20,7 +20,7 @@
 
 
     <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>'
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 
     <link href="css/base.css" type="text/css" rel="stylesheet"/>
