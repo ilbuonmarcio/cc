c0a7b3ae70659f933db981866be75c19cce23657
marcioz98 <marciozgaming@gmail.com>
2018-04-04 08:42:03 +0200

    Changed file permissions

--- src/config/dbconfig.php
+++ src/config/dbconfig.php
@@ -2,6 +2,6 @@
   // Definisco le costanti per la connessione al database
   define("HOST", "localhost");
   define("DB_USER", "root");
-  define("DB_PASSWORD", "");
+  define("DB_PASSWORD", "123");
   define("DB_NAME", "composizioneclassi");
 ?>
