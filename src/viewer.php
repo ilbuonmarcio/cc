185488fbfe09c4e2ada1d16fbb1bbf6b1f06c270
marcioz98 <marciozgaming@gmail.com>
2018-04-05 21:50:20 +0200

    Fixed coding indentation (erPise f u) & added link to home page if table is empty

--- src/viewer.php
+++ src/viewer.php
@@ -28,81 +28,82 @@
 
   <body>
 
-	 <?php
+	<?php
 
-			  if(isset($_GET["groupid"])){
+	  if(isset($_GET["groupid"])){
 
-				  $groupid = $_GET["groupid"];
+		  $groupid = $_GET["groupid"];
 
-				  include("utils/dbconnection.php");
+		  include("utils/dbconnection.php");
 
-				  $conn = connectdb();
+		  $conn = connectdb();
 
-				  if($conn){
+		  if($conn){
 
-					$query = "SELECT * FROM alunni WHERE id_gruppo = " . $groupid . ";";
+				$query = "SELECT * FROM alunni WHERE id_gruppo = " . $groupid . ";";
 
-					$result = $conn->query($query);
+				$result = $conn->query($query);
 
-					if ($result->num_rows > 0) {
-					  echo '<table style="box-shadow: 1px 1px 10px #BBBBBB;" class="striped centered">
-						<thead>
-						  <tr>
-							  <th>Cognome</th>
-							  <th>Nome</th>
-							  <th>Matricola</th>
-							  <th>Codice Fiscale</th>
-							  <th>Desiderata</th>
-							  <th>Sesso</th>
-							  <th>Data di nascita</th>
-							  <th>Cap</th>
-							  <th>Nazionalita</th>
-							  <th>Legge 107</th>
-							  <th>Legge 104</th>
-							  <th>Classe precedente</th>
-							  <th>Classe sucessiva</th>
-							  <th>Anno scolastico</th>
-							  <th>Scelta indirizzo</th>
-							  <th>Codice catastale</th>
-							  <th>Voto</th>
-							  <th>Id gruppo</th>
-						  </tr>
-						</thead>
+				if ($result->num_rows > 0) {
+				  echo '<table style="box-shadow: 1px 1px 10px #BBBBBB;" class="striped centered">
+      						<thead>
+      						  <tr>
+      							  <th>Cognome</th>
+      							  <th>Nome</th>
+      							  <th>Matricola</th>
+      							  <th>Codice Fiscale</th>
+      							  <th>Desiderata</th>
+      							  <th>Sesso</th>
+      							  <th>Data di nascita</th>
+      							  <th>Cap</th>
+      							  <th>Nazionalita</th>
+      							  <th>Legge 107</th>
+      							  <th>Legge 104</th>
+      							  <th>Classe precedente</th>
+      							  <th>Classe sucessiva</th>
+      							  <th>Anno scolastico</th>
+      							  <th>Scelta indirizzo</th>
+      							  <th>Codice catastale</th>
+      							  <th>Voto</th>
+      							  <th>Id gruppo</th>
+      						  </tr>
+      						</thead>
+      					<tbody>';
 
-						<tbody>';
-					  while($row = $result->fetch_assoc()) {
-						echo '<tr>
-								<td>' . $row["cognome"]           . '</td>
-								<td>' . $row["nome"]              . '</td>
-								<td>' . $row["matricola"]         . '</td>
-								<td>' . $row["cf"]                . '</td>
-								<td>' . $row["desiderata"]        . '</td>
-								<td>' . $row["sesso"]             . '</td>
-								<td>' . $row["data_nascita"]      . '</td>
-								<td>' . $row["cap"]               . '</td>
-								<td>' . $row["nazionalita"]       . '</td>
-								<td>' . $row["legge_107"]         . '</td>
-								<td>' . $row["legge_104"]         . '</td>
-								<td>' . $row["classe_precedente"] . '</td>
-								<td>' . $row["classe_successiva"]  . '</td>
-								<td>' . $row["anno_scolastico"]   . '</td>
-								<td>' . $row["scelta_indirizzo"]  . '</td>
-								<td>' . $row["cod_cat"]           . '</td>
-								<td>' . $row["voto"]              . '</td>
-								<td>' . $row["id_gruppo"]         . '</td>
-							  </tr>';
-					  }
-					  echo '
-					</tbody>
-				  </table>';
-					}
-				  } else{
-					echo '<p class="center-align" style="color: red;">Tabella non disponibile! Errore di connessione al database.</p>';
+			  while($row = $result->fetch_assoc()) {
+					echo '<tr>
+							<td>' . $row["cognome"]           . '</td>
+							<td>' . $row["nome"]              . '</td>
+							<td>' . $row["matricola"]         . '</td>
+							<td>' . $row["cf"]                . '</td>
+							<td>' . $row["desiderata"]        . '</td>
+							<td>' . $row["sesso"]             . '</td>
+							<td>' . $row["data_nascita"]      . '</td>
+							<td>' . $row["cap"]               . '</td>
+							<td>' . $row["nazionalita"]       . '</td>
+							<td>' . $row["legge_107"]         . '</td>
+							<td>' . $row["legge_104"]         . '</td>
+							<td>' . $row["classe_precedente"] . '</td>
+							<td>' . $row["classe_successiva"] . '</td>
+							<td>' . $row["anno_scolastico"]   . '</td>
+							<td>' . $row["scelta_indirizzo"]  . '</td>
+							<td>' . $row["cod_cat"]           . '</td>
+							<td>' . $row["voto"]              . '</td>
+							<td>' . $row["id_gruppo"]         . '</td>
+						  </tr>';
 				  }
-			  }
+  			  echo '</tbody>
+  				    </table>';
 
+        } else {
+          echo '<p class="center-align" style="color: red;">Tabella vuota! <a href="index.php">Clicca qui per tornare alla home</a></p>';
+        }
+			} else{
+			  echo '<p class="center-align" style="color: red;">Tabella non disponibile! Errore di connessione al database.</p>';
+			}
+		}
 
-            ?>
+  ?>
 
   </body>
 </html>
