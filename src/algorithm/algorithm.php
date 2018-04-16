8d01575c1de91cd6b37de00fffede4fb438e6984
Spollo <damivise@gmail.com>
2018-04-16 12:53:41 +0200

    Adjusted indentation of algorithm.php

--- src/algorithm/algorithm.php
+++ src/algorithm/algorithm.php
@@ -1,12 +1,32 @@
 <?php
-	include("../utils/dbconnection.php");
+	include("../utils/db.php");
 	
 	class EmptyGroupException extends Exception { }
 	class NoConfigFoundException extends Exception { }
 
 	Class Alunno{
 		
-		function __construct($id, $cognome, $nome, $matricola, $cf, $desiderata, $sesso, $data_nascita, $cap, $nazionalita, $legge_170, $legge_104, $classe_precedente, $classe_successiva, $scelta_indirizzo, $cod_cat, $voto, $id_gruppo){
+		function __construct(
+						$id, 
+						$cognome,
+						$nome, 
+						$matricola, 
+						$cf, 
+						$desiderata, 
+						$sesso, 
+						$data_nascita,
+						$cap, 
+						$nazionalita,
+						$legge_170, 
+						$legge_104, 
+						$classe_precedente, 
+						$classe_successiva,
+						$scelta_indirizzo, 
+						$cod_cat, 
+						$voto, 
+						$id_gruppo
+				){
+			
 			$this->id = $id;
 			$this->cognome = $cognome;
 			$this->nome = $nome;
@@ -41,20 +61,36 @@
 		function fillStudentsArrayFromDB($groupid){
 			$conn = connectdb();
 			$query = "SELECT * FROM alunni WHERE id_gruppo = ". $groupid .";";
-
 			$result = $conn->query($query);
-			
 			$tmparray = array();
 			
 			if ($result->num_rows > 0) {
 				while($row = $result->fetch_assoc()){
-					
-					array_push($tmparray, new Alunno($row["id"], $row["cognome"], $row["nome"], $row["matricola"], $row["cf"], $row["desiderata"], $row["sesso"], $row["data_nascita"], $row["cap"], $row["nazionalita"],$row["legge_107"], $row["legge_104"], $row["classe_precedente"], $row["classe_successiva"], $row["scelta_indirizzo"], $row["cod_cat"], $row["voto"], $row["id_gruppo"]));
-					
+					array_push(
+						$tmparray, 
+						new Alunno(
+							$row["id"], 
+							$row["cognome"], 
+							$row["nome"], 
+							$row["matricola"],
+							$row["cf"],
+							$row["desiderata"], 
+							$row["sesso"], 
+							$row["data_nascita"], 
+							$row["cap"], 
+							$row["nazionalita"],
+							$row["legge_107"], 
+							$row["legge_104"], 
+							$row["classe_precedente"], 
+							$row["classe_successiva"], 
+							$row["scelta_indirizzo"], 
+							$row["cod_cat"], 
+							$row["voto"], 
+							$row["id_gruppo"]
+						)
+					);
 				}
-				
 				$this->students = $tmparray;
-				
 			}else{
 				throw new EmptyGroupException("Empty result set (loadStudents)");
 			}
@@ -73,9 +109,9 @@
 	}
 	
 	Class ClassiComposte{
-		/********************************
-		*				ROBBE			*
-		*********************************/
+		/***********************************
+		*				ROBBE			   *
+		************************************/
 	}
 	
 	Class Config{
@@ -111,7 +147,6 @@
 				throw new NoConfigFoundException("Empty result set (loadConfigCC)");
 			}
 		}
-		
 	}
 	
 	Class CC{
