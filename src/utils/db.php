36c9a75ea6e178a86af7599f0ea5b6e3df719770
Alessandro Marchioro <marciozgaming@gmail.com>
2018-05-11 08:36:29 +0200

    Uploaded standard db.php

---
+++ src/utils/db.php
@@ -0,0 +1,21 @@
+ <?php
+
+  // Definisco le costanti per la connessione al database
+
+  define("HOST", "localhost");
+  define("DB_USER", "root");
+  define("DB_PASSWORD", "");
+  define("DB_NAME", "composizioneclassi");
+
+  /*
+    Funzione che ritorna un istanza di connessione al database, se disponibile
+  */
+	function connectDB() {
+		$conn = new mysqli(HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+		if ($conn->connect_error) {
+			header("Location: ../index.php");
+		}
+		return $conn;
+	}
+?>
