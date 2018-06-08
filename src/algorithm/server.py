e592f1a6b2c61ceaba893499cf985e9dfbd804c6
marcioz98 <marciozgaming@gmail.com>
2018-06-08 18:32:56 +0200

    Added dynamic select using flask backjed

--- src/algorithm/server.py
+++ src/algorithm/server.py
@@ -1,7 +1,10 @@
 import CC
 import flask
+import json
+import mysql.connector
 from flask_cors import CORS
 from multiprocessing.pool import ThreadPool
+from components.DBConfig import DBConfig
 
 app = flask.Flask(__name__)
 CORS(app)
@@ -12,3 +15,55 @@ def get_cc_result():
     async_result = pool.apply_async(CC.create_cc_instance, (0, 1, 1))
     return_val = async_result.get()
     return return_val
+
+@app.route('/refresh_configurecc_configid_select', methods=['GET'])
+def refresh_configurecc_configid_select():
+    connection = mysql.connector.connect(
+                    user=DBConfig.user,
+                    password=DBConfig.password,
+                    host=DBConfig.host,
+                    database=DBConfig.database)
+
+    cursor = connection.cursor()
+
+    query = "SELECT id, nome FROM configurazioni;"
+
+    cursor.execute(query)
+
+    configurations = cursor.fetchall()
+
+    str_response = ""
+    for configuration in configurations:
+        str_response += '<option value="' + str(configuration[0]) + '">' + configuration[1] + '</option>'
+
+    cursor.close()
+
+    connection.close()
+
+    return str_response
+
+@app.route('/refresh_configurecc_groupid_select', methods=['GET'])
+def refresh_configurecc_groupid_select():
+    connection = mysql.connector.connect(
+                    user=DBConfig.user,
+                    password=DBConfig.password,
+                    host=DBConfig.host,
+                    database=DBConfig.database)
+
+    cursor = connection.cursor()
+
+    query = "SELECT id, nome FROM gruppi;"
+
+    cursor.execute(query)
+
+    groups = cursor.fetchall()
+
+    str_response = ""
+    for group in groups:
+        str_response += '<option value="' + str(group[0]) + '">' + group[1] + '</option>'
+
+    cursor.close()
+
+    connection.close()
+
+    return str_response
