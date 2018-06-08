b9494a4d310b5cd7d08c0352eb11c342b2b95ca2
marcioz98 <marciozgaming@gmail.com>
2018-06-08 17:08:19 +0200

    Changed classes structure

--- src/algorithm/server.py
+++ src/algorithm/server.py
@@ -1,6 +1,6 @@
 import multiprocessing
-import algorithm
+import CC
 
 if __name__ == "__main__":
-    p = multiprocessing.Process(target=algorithm.create_cc_instance, args=(0, 1, 1))
+    p = multiprocessing.Process(target=CC.create_cc_instance, args=(0, 1, 1))
     p.start()
