ebb61fe50c5648c8239a5fe2933af9cc841bc1b1
marcioz98 <marciozgaming@gmail.com>
2018-06-15 13:55:30 +0200

    Added export GET

--- src/algorithm/server.py
+++ src/algorithm/server.py
@@ -1,5 +1,5 @@
 import CC
-from flask import Flask, request
+from flask import Flask, request, Response
 import json
 import mysql.connector
 from flask_cors import CORS
@@ -125,3 +125,41 @@ def refresh_visualizecc_table():
     str_response += "</tbody></table>"
 
     return str_response
+
+@app.route('/export_generatedcc_to_csv', methods=['GET'])
+def export_generatedcc_to_csv():
+
+    connection = mysql.connector.connect(
+                    user=DBConfig.user,
+                    password=DBConfig.password,
+                    host=DBConfig.host,
+                    database=DBConfig.database)
+
+    cursor = connection.cursor()
+
+    groupid = request.args.get('groupid')
+    configid = request.args.get('configid')
+
+    query = f"SELECT classid, matricola, cognome, nome, sesso, voto FROM classi_composte LEFT JOIN alunni ON alunni.id = classi_composte.studentid WHERE groupid = {groupid} AND configid = {configid} ORDER BY classid, cognome, nome;"
+
+    cursor.execute(query)
+
+    records = cursor.fetchall()
+
+    cursor.close()
+
+    connection.close()
+
+    generator = (
+        str(record)
+        .replace(',', ';')
+        .replace("'", "")
+        .replace('(', '')
+        .replace(')', '') + "\n"
+        for record in records
+    )
+
+    return Response(generator,
+                       mimetype="text/plain",
+                       headers={"Content-Disposition":
+                                    "attachment;filename=test.csv"})
