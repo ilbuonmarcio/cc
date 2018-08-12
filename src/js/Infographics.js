844524cc22d46bd5cce1e22cfd3ec4547dfe4019
Spollo <damivise@gmail.com>
2018-08-12 11:30:30 +0200

    Added graphs for the number of Males/Females per Class and Average of all Classes

--- src/js/Infographics.js
+++ src/js/Infographics.js
@@ -61,13 +61,29 @@ function orderStudentsByMark(students, marks){
 			resultObject["marks"].push(i);
 		});
 	}
-	console.log(resultObject);
 
 	return resultObject;
 }
 
 function getNumberOfValuesJson(json_data) {
 	var result = {};
+	
+	for(var i = 0; i < json_data.length; i++){
+		
+		if(!result[json_data[i]]){
+			result[json_data[i]] = 1;
+		}else{
+			result[json_data[i]] = result[json_data[i]] + 1;
+		}
+	}
+	
+	return result;
+}
+
+function getNumberOfMalesFemales(json_data) {
+	var result = {};
+	result['m'] = 0;
+	result['f'] = 0;
 	
 	for(var i = 0; i < json_data.length; i++){
 		
@@ -118,6 +134,17 @@ $.getJSON(
     studentNaz = result;
 });
 
+var studentMaleFemale = null;
+$.getJSON(
+	"http://127.0.0.1:5000/get_charts_data_male_female?groupid=" 
+	+ getParamFromURL('groupid') 
+	+ "&configid="
+	+ getParamFromURL('configid'), 
+	
+	function(result){
+    studentMaleFemale = result;
+});
+
 $.ajaxSetup({
    async: true
 });
@@ -126,6 +153,7 @@ $.ajaxSetup({
 var json_data_mark = getValues(studentMark);
 var json_data_cap = getValues(studentCap);
 var json_data_naz = getValues(studentNaz);
+var json_data_male_female = getValues(studentMaleFemale);
 
 // Get the number of total classes
 var number_of_classes = json_data_mark[0].length;
@@ -145,6 +173,13 @@ var class_chart_canvases_naz = Array(number_of_classes);
 for(let i = 0; i < number_of_classes; i++){
 	class_chart_canvases_naz[i] = document.getElementById('class-chart-' + String(i+1+(number_of_classes*2))).getContext('2d');
 }
+
+var class_chart_canvases_male_female = Array(number_of_classes);
+for(let i = 0; i < number_of_classes; i++){
+	class_chart_canvases_male_female[i] = document.getElementById('class-chart-' + String(i+1+(number_of_classes*3))).getContext('2d');
+}
+
+var class_chart_canvases_classes_mark = document.getElementById('class-chart-' + String(1+(number_of_classes*4))).getContext('2d');
 
 // Creating the data to use in charts
 var data_array_mark = Array(number_of_classes);
@@ -198,6 +233,55 @@ for(let i = 0; i < number_of_classes; i++){
 	data_array_naz.push(data);
 }
 
+var data_array_male_female = Array(number_of_classes);
+for(let i = 0; i < number_of_classes; i++){
+	var dataMalesFemales = getNumberOfMalesFemales(json_data_male_female[2][i]);
+	var data = {
+	  labels: [
+		'Maschi',
+		'Femmine'
+	  ],
+	  datasets: [
+		{
+		  data: Object.values(dataMalesFemales), // [26, 2]
+		  backgroundColor: color_palette
+		}
+	  ]
+	};
+	data_array_male_female.push(data);
+}
+
+
+var data_array_classes_mark;
+console.log(json_data_mark);
+var average = Array();
+
+for(let i = 0; i < number_of_classes; i++){
+	var sum = 0;
+	
+	for(var l = 0; l < json_data_mark[2][i].length; l++){
+		
+		sum += json_data_mark[2][i][l]
+	}
+	
+	average.push(sum/json_data_mark[2][i].length);
+}
+
+console.log(average);
+
+var data = {
+		labels: json_data_mark[0], // ["17219", "17130"],
+		datasets: [
+		{
+			label: "Media Voti Classi", // 'Cap per Classe',
+			data: average, // [21234, 31312],
+			backgroundColor: color_palette
+		}
+	]
+};
+data_array_classes_mark = data;
+
+
 // Option of the charts
 var options = {
   scales: {
@@ -239,3 +323,18 @@ for(let i = 0; i < number_of_classes; i++){
         options: options
     });
 };
+
+var classes_charts_male_female = Array(number_of_classes);
+for(let i = 0; i < number_of_classes; i++){
+	classes_charts_male_female[i] = new Chart(class_chart_canvases_male_female[i], {
+        type: 'pie',
+        data: data_array_male_female[number_of_classes + i],
+        options: Chart.defaults.pie
+    });
+};
+
+var classes_charts_classes_mark = new Chart(class_chart_canvases_classes_mark, {
+    type: 'bar',
+    data: data_array_classes_mark,
+    options: options
+});
